5554bcec2bc5a755517eb8739791f4a0c66c0a54
Alicia Barrios <aliciabgo@gmail.com>
2022-09-09 10:23:17 +0200

    cambios en el form

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const url = document.querySelector("form.action");
 
-const datos = {
-    name:
-    email:
-    phone:
-    message:
-},
+const datos = new FormData(document.getElementById('formulario'));
 
 const body = { item: datos };
 
@@ -17,23 +12,33 @@ const fetchParams = {
         "X-API-Key": 'a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H'
     },
     body: JSON.stringify(body)
-}
+};
 
-async function enviarFormulario() {
-    try {
-        let response = await fetch(url, fetchParams);
-        if (response.ok) {
-            let respuesta = await response.json();
-            result.innerHTML = "El formulario se ha enviado correctamente";
-            datos.reset();
-        } else {
-            throw new Error(response.statusText);
-        }
-    } catch (err) {
-        result.innerHTML = "Error al enviar el formulario";
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("form").addEventListener("submit", enviarFormulario);
+fetch(url, fetchParams)
+    .then(response => {
+        if (response.ok) return response.json();
+    })
+    .then(json => {
+        console.log(json);
     });
+
+// async function enviarFormulario() {
+//     try {
+//         let response = await fetch(url, fetchParams);
+//         if (response.ok) {
+//             let respuesta = await response.json();
+//             console.log(respuesta);
+//             result.innerHTML = "El formulario se ha enviado correctamente";
+//             datos.reset();
+//         } else {
+//             throw new Error(response.statusText);
+//         }
+//     } catch (err) {
+//         result.innerHTML = "Error al enviar el formulario";
+//         console.log("Error");
+//     }
+// }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.querySelector('#submit').addEventListener('click', enviarFormulario);
+//     });
